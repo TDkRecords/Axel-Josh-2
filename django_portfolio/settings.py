dd8bc0294cccbcea65beca72b540b411bc01690f
TDkRecords <126419482+TDkRecords@users.noreply.github.com>
2023-11-21 22:11:14 -0500

    Update settings.py
    aun intentando la database para vercel

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
         'OPTIONS': {
-            'sqlite_deterministic': False,
+            'sqlite_deterministic': os.environ.get('SQLITE_DETERMINISTIC', False),
         }
     }
 }
